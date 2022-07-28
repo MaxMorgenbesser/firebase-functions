48c78f5dfff59477981a5191c04c6db4a2c4b2e7
Max Morgenbesser <maxwellmorgenbesser@gmail.com>
2022-07-28 15:48:53 -0400

    connected api to cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,22 @@
 const functions = require("firebase-functions");
-
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
 //exports.helloThere=functions.https.onRequest
 
+exports.helloThere = functions.https.onRequest((req, res) => {
+  res.send("Hello there Firebase cloud user");
+});
 
-    exports.helloThere=functions.https.onRequest((req,res) =>
-    {res.send('Hello there Firebase cloud user')
+//setup routes
+app.get('/test', (req,res)=>{
+    res.send("it actually worked")
 })
+app.get('/another-one',(req,res)=>{
+    res.send('this works too')
+})
+exports.api= functions.https.onRequest(app);
 
 
 // // Create and Deploy Your First Cloud Functions
@@ -15,5 +26,3 @@ const functions = require("firebase-functions");
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
-
-
